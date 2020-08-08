a50e87183f8a24abda202047dbdcce38c65065d0
Rajeev Jaiswal <rajeevjaiswal26@gmail.com>
2020-08-08 23:27:42 +0530

    Added required dependencies. Removed default boilerplate code

---
+++ lib/constants/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final ThemeData themeData = new ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.green,
+    primaryColorBrightness: Brightness.light,
+    accentColorBrightness: Brightness.light);
---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Heady"),
+      ),
+      body: Container(
+        color: Colors.red,
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
